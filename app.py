b8964a72667b5d0a21132b70c209bcb041db1370
DeniHidayatullah <denih1360@gmail.com>
2024-07-03 12:22:12 +0700

    Menambahkan Luas Lingkaran

--- app.py
+++ app.py
@@ -14,3 +14,12 @@ def luas_persegi_panjang():
   print(f"Luas: {luas}")
 
 luas_persegi_panjang()
+
+# Hitung Luas Persegi Panjang
+def luas_lingkaran():
+  phi = 3.14
+  r = int(input("Masukan Jari-jari: "))
+  luas = phi * r * r
+  print(f"Luas: {luas}")
+
+luas_lingkaran()
